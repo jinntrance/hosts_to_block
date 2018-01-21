ae1cdec76b952a100d9284bffd5c8bbe098ebb9b
Joseph J.C. Tang <jinntrance@gmail.com>
2018-01-22 00:56:55 +0800

    make it work in Proxifier

--- simplify.sh
+++ simplify.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-awk 'NF==2 && $1=="0.0.0.0" {print $2}' hosts/alternates/fakenews-gambling-porn/hosts | rev | sort | uniq | rev
+awk 'NF==2 && $1=="0.0.0.0" {print $2}' hosts/alternates/fakenews-gambling-porn/hosts | rev | sort | uniq | rev > full_hosts
+sed 's/[^a-zA-Z0-9.]\{1,\}/*/g' full_hosts | grep -v '[^a-zA-Z0-9.*]' > simplified_hosts
+
